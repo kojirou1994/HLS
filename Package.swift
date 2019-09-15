2bdc91276bdf6d9e249c41d74f1a3b7b2427bb50
kojirou <kojirouhtc@gmail.com>
2019-09-15 13:30:38 +0800

    update

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["HLS"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kojirou1994/Kwift.git", from: "0.2.0"),
+        .package(url: "https://github.com/kojirou1994/Kwift.git", from: "0.3.1"),
         .package(url: "git@github.com:kojirou1994/Remuxer.git", from: "0.0.1"),
         .package(url: "https://github.com/IBM-Swift/swift-html-entities.git", .branch("master")),
         .package(url: "https://github.com/kojirou1994/URLFileManager.git", from: "0.0.1"),
